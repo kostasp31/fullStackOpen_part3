f9bb34601900d4e503f8eb018e2ef8a0824109bd
kostasp31 <kostaspetrakis2003@gmail.com>
2024-08-04 00:59:44 +0300

    3.1 - 3.6

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+  
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+app.get('/info', (request, response) => {
+    const now = new Date()
+    response.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${now.toString()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = phonebook.find(person => person.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    phonebook = phonebook.filter(person => person.id !== id)
+
+    response.status(204).send()
+})
+
+const randInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.post('/api/persons', (request, response) => {
+    const bd = request.body
+    if (!bd.name && !bd.number) {
+        return response.status(400).json({
+            error: 'name and number missing'
+        })
+    }
+    if (!bd.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+    if (!bd.number) {
+      return response.status(400).json({ 
+        error: 'number missing' 
+      })
+    }
+
+    const twin = phonebook.find(person => person.name === bd.name)
+    if (twin) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+  
+    const person = {
+      name: bd.name,
+      number: bd.number,
+      id: `${randInt(0, 100000)}`
+    }
+    phonebook = phonebook.concat(person)
+  
+    response.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
